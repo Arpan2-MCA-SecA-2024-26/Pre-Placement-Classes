83bfc3e8b641e84315bab694c0c4e3b33fca6eca
Arpan Malakar <arpanmalakar4@gmail.com>
2025-02-23 13:25:33 +0530

    Update Pro1.py

--- Assignment - 3_2nd Sem/Pro1.py
+++ Assignment - 3_2nd Sem/Pro1.py
@@ -1,4 +1,4 @@
-//1. Write a program to input a 4 digit number and print the even and odd digits and total even and odd digits.
+#1. Write a program to input a 4 digit number and print the even and odd digits and total even and odd digits.
 
 num = input("Enter a 4-digit number: ")
 even_digits = [int(digit) for digit in num if int(digit) % 2 == 0]
